b4731a04133c872f29add196846f9b6a553d3f62
Bjenhamin4Alfredolvchenki <66851510+Bjenhamin4Alfredolvchenki@users.noreply.github.com>
2022-02-21 21:26:53 +0000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/** @type {import('astro').AstroUserConfig} */
+export default {
+  renderers: [],
+  vite: {
+    plugins: [],
+  },
+  buildOptions: {
+    site: "https://astro-theme-creek.netlify.app/",
+    sitemap: true,
+  },
+};
